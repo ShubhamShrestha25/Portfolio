90f12d57131e697cd57935ecdadc345fc6b4934c
Shubham Shrestha <shubhamshrestha25@gmail.com>
2020-12-05 18:23:00 +0545

    adding hamburgur menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Navbar.css";
 
 const Navbar = () => {
+
+        const [click, setClick] = useState(false);
+        
+        const clickHandle = () => setClick (!click);
+
     return (
         <div className="navbarr">
             <div className="left"> 
             <h1>PortFolio</h1>
             </div>
-            <div className="right">
+            
+            <div className={click? "right active" : "right"}>
             <h1 >Home</h1>
             <h1 >About</h1>
             <h1 >Skills</h1>
             <h1 >Contact</h1>
             </div>
-            <div className="hamburger" >
-                <div className="line1"></div>
-                <div className="line2"></div>
-                <div className="line3"></div>
+            
+            <div className="hamburger" onClick={clickHandle}>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
             </div>
             
         </div>
     )
+    
 }
 
 export default Navbar
